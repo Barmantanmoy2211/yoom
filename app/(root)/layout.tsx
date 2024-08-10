098408a0b74adb5aeaededb996967e6113034740
Barmantanmoy2211 <barmantanmoy59@gmail.com>
2024-08-10 10:24:53 +0530

    for development

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,22 @@
+import StreamVideoProvider from "@/provider/StreamClientProvider"
+import { Metadata } from "next";
 import { ReactNode } from "react"
+
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "A video call app",
+  icons: {
+    icon: '/icons/yoom-logo.svg',
+  }
+};
 
 
 const RootLayout = ({children} : { children: ReactNode}) => {
   return (
     <main>
+      <StreamVideoProvider>
         {children}
+      </StreamVideoProvider>
     </main>
   )
 }
